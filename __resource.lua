fc2420ace57bfe699f71be57606cf7c80f6a8309
Noah <103244260+NoNoTheGoat@users.noreply.github.com>
2022-07-20 13:02:17 -0700

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,7 +1,7 @@
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
-author "Noah L From Hunter's Development"
-description " This Script Will Kick People That Say The Blacklisted Words. (In sc_blwords.lua) https://discord.gg/ZhNtejyzEy"
-website "https://discord.gg/ZhNtejyzEy"
+author "NoNo"
+description " This Script Will Kick People That Say The Blacklisted Words. (In sc_blwords.lua)"
+website ""
 
 server_scripts{ 
     'sv_blwords.lua',
